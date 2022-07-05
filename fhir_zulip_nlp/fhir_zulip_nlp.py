243deb91a58ca2e76e5d5c6ad834fa741ebe7e16
joeflack4 <joeflack4@gmail.com>
2022-07-05 17:52:51 -0400

    - Requirements: Added 'spyder_kernels' to requirements, which enables the Spyder IDE console to work. - Bugfix: Updated file paths to be relative; should work on anyone's setup now. - Refactor: (i) variable renames, (ii) code style standardization, (iii) removed unused functions, (iv) rearranged order of some code.

--- fhir_zulip_nlp/fhir_zulip_nlp.py
+++ fhir_zulip_nlp/fhir_zulip_nlp.py
@@ -11,7 +11,7 @@ Possible areas of improvement
   1. Save to calling directory, not project directory.
 """
 import os
-from typing import Dict
+from typing import Dict, List
 
 import pandas as pd
 import zulip
@@ -21,103 +21,25 @@ from datetime import datetime
 PKG_DIR = os.path.dirname(os.path.realpath(__file__))
 PROJECT_DIR = os.path.join(PKG_DIR, '..')
 CONFIG = {
-    'outdir': PROJECT_DIR,
-    'report1_filename': 'fhir-zulip-nlp - frequencies and date ranges.csv',
-    'report2_filename': 'fhir-zulip-nlp - age and date analyses.csv',
-    'zuliprc_path': os.path.join(PROJECT_DIR, 'zuliprc')  # rc = "runtime config"
+    # 'outdir': PROJECT_DIR,
+    # 'report1_filename': 'fhir-zulip-nlp - frequencies and date ranges.csv',
+    # 'report2_filename': 'fhir-zulip-nlp - age and date analyses.csv',
+    'zuliprc_path': os.path.join(PROJECT_DIR, 'zuliprc'),  # rc = "runtime config"
+    'outpath': os.path.join(PROJECT_DIR, 'zulip_report.csv')
 }
 
 
-# todo: maybe turn into a class or decorator to share common logic
 # Functions
-def analysis_frequencies(data, outpath: str = None) -> pd.DataFrame:
-    """Analyze counts and date ranges
-    Requirements 1-5
-    """
-    print(data)
-    df = pd.DataFrame()
-    
-    if outpath:
-        df.to_csv(outpath, index=False)
-    return df
-
-
-def analysis_age_and_date(data, outpath: str = None) -> pd.DataFrame:
-    """Analyze age and date
-    Requirements 6
-    """
-    print(data)
-    df = pd.DataFrame()
-
-    if outpath:
-        df.to_csv(outpath, index=False)
-    return df
-
-
-def normalize_data(data):
-    """Normalize data"""
-    return data
-
-
-# TODO: will probably want to pass (i) chat.fhir.org, (ii) list of streams
-def data(key):
-    """Fetch data from API
-    Resources
-      - Get all streams (probably not needed): https://zulip.com/api/get-streams
-    """    
-    bob = False
-    i = 1
-    while True:
-        if bob:
-            break
-        res = messagepull(0,0,i,key)
-        allmess = res['messages']
-        i+=1
-        bob = res['found_newest']
-    
-    newest = timechange(messagepull('newest',1,0,key)['messages'][0]['timestamp'])
-    oldest = timechange(messagepull('oldest',0,1,key)['messages'][0]['timestamp'])
-    
-    vals = {'name': key, 'number of occurences': len(allmess), 'most recent message': newest, 'oldest message' : oldest}
-    return vals
-
-def messagepull(anchor,before,after,keyword):
-    client = zulip.Client(config_file="~/fhir-zulip-nlp-analysis/zuliprc")
-    request = {
-            "anchor": anchor,
-            "num_before": before,
-            "num_after": after,
-            "narrow": [
-                {"operator": "stream", "operand": "terminology"},
-                {"operator": "search", "operand": keyword},
-                ],
-            }
-    result = client.get_messages(request)
-    return result 
-
-def timechange(i): 
-    return datetime.utcfromtimestamp(i).strftime('%Y-%m-%d %H:%M:%S')
-
-def alldicts(lis):
-    message_objects = []
-    for i in lis:
-        x = data(i)
-        message_objects.append(x)
-    newmessages = sorted(message_objects, key=lambda d: d['number of occurances'])
-    df = pd.DataFrame(newmessages)
-    df.to_csv('~/fhir-zulip-nlp-analysis-main/zulip_report.csv', index=False)
-    return newmessages
-
 '''
 def data(key):
-    client = zulip.Client(config_file="~/fhir-zulip-nlp-analysis-main/zuliprc")
+    client = zulip.Client(config_file=CONFIG['zuliprc_path'])
     vals = {}
     mess = get_messages_and_topics(key)
-    
+
     vals['name'] = key
     vals['number of occurances']=len(mess)
-    
-    
+
+
     newest = {
         "anchor": "newest",
         "num_before": 1,
@@ -131,7 +53,7 @@ def data(key):
     datenew = result_new['messages'][0]['timestamp']
     datenew1 = datetime.utcfromtimestamp(datenew).strftime('%Y-%m-%d %H:%M:%S')
     vals['newest time']=datenew1
-    
+
     oldest = {
         "anchor": "oldest",
         "num_before": 0,
@@ -145,51 +67,77 @@ def data(key):
     dateold = result_old['messages'][0]['timestamp']
     dateold1 = datetime.utcfromtimestamp(dateold).strftime('%Y-%m-%d %H:%M:%S')
     vals['oldest time']=dateold1
-    
+
     return vals
 '''
 
-def run(outdir: str, report1_filename: str, report2_filename: str, zuliprc_path: str) -> Dict[str, pd.DataFrame]:
-    """Run analysis"""
-    # Collect and normalize data
-    # TODO: (i) get data, (ii) what format to normalize into? dict? df?
-    #part 1 of project
-    '''
-    code_sys_names = ['DICOM','SNOMED','LOINC','ICD','NDC','RxNorm']
+
+def messagepull(anchor: str, before: int, after: int, keyword: str) -> Dict:
+    """Get messages"""
+    client = zulip.Client(config_file=CONFIG['zuliprc_path'])
+    request = {
+            "anchor": anchor,
+            "num_before": before,
+            "num_after": after,
+            "narrow": [
+                {"operator": "stream", "operand": "terminology"},
+                {"operator": "search", "operand": keyword},
+            ],
+            }
+    result = client.get_messages(request)
+    return result 
+
+
+def format_timestamp(i):
+    """Format datetime string"""
+    return datetime.utcfromtimestamp(i).strftime('%Y-%m-%d %H:%M:%S')
+
+
+# TODO: will probably want to pass (i) chat.fhir.org, (ii) list of streams
+def get_data(key):
+    """Fetch data from API
+    Resources
+      - Get all streams (probably not needed): https://zulip.com/api/get-streams
+    """
+    bob = False
+    i = 1
+    allmess: List = []
+    while True:
+        if bob:
+            break
+        res = messagepull(str(0), 0, i, key)
+        allmess = res['messages']
+        i += 1
+        bob = res['found_newest']
+
+    newest = format_timestamp(messagepull('newest', 1, 0, key)['messages'][0]['timestamp'])
+    oldest = format_timestamp(messagepull('oldest', 0, 1, key)['messages'][0]['timestamp'])
+
+    vals = {'name': key, 'number of occurences': len(allmess), 'most recent message': newest, 'oldest message': oldest}
+    return vals
+
+
+def alldicts(lis):
+    """Get all dictionaries"""
     message_objects = []
-    data(code_sys_names[0])
-    
-    for i in code_sys_names:
-        curr = get_messages_and_topics(i)
-        message_objects.append(curr)
-    '''
-         
-    
-
-    # Analyze
-    df1 = analysis_frequencies( os.path.join(outdir, report1_filename))
-    df2 = analysis_age_and_date( os.path.join(outdir, report2_filename))
-
-    # Return
-    report = {
-        report1_filename: df1,
-        report2_filename: df2
-    }
-    return report
+    for i in lis:
+        data = get_data(i)
+        message_objects.append(data)
+    newmessages = sorted(message_objects, key=lambda d: d['number of occurances'])
+    df = pd.DataFrame(newmessages)
+    df.to_csv(CONFIG['outpath'], index=False)
+    return newmessages
 
 
 # Execution
 if __name__ == '__main__':
-    run(**CONFIG)
-    codes = ['DICOM','SNOMED','LOINC','ICD','NDC','RxNorm']
+    code_systems = ['DICOM', 'SNOMED', 'LOINC', 'ICD', 'NDC', 'RxNorm']
     # message_objects = []
     # for i in code_sys_names:
     #     x = data(i)
     #     message_objects.append(x)
     # newmessages = sorted(message_objects, key=lambda d: d['number of occurances'])
     # df = pd.DataFrame(newmessages)
-    # df.to_csv('~/fhir-zulip-nlp-analysis-main/zulip_report.csv', index=False)
-    x = alldicts(codes)
-    print(x)     
-    
-   
+    # df.to_csv(CONFIG['outpath'], index=False)
+    all_results = alldicts(code_systems)
+    print(all_results)
